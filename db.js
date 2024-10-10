fe2decc7882b96ec3713c65a231375b86a451f37
Yassin1-prog <132544868+Yassin1-prog@users.noreply.github.com>
2024-10-10 16:28:52 +0300

    config production db

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL // use Railway's DATABASE_URL in production
+    : process.env.LOCAL_DATABASE_URL; // use local DB for development
+
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connectionString,
 });
